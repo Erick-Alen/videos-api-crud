b82bd109636dfc78af36adb7415848ca91fe98b9
Erick Alen <erick.alen@live.com>
2023-09-09 00:39:34 -0300

    Node API with fastify ising Postgre

---
+++ server.js
@@ -0,0 +1,60 @@
+// import {createServer} from 'node:http'
+
+
+// const server = createServer((request, response) => {
+  //   response.write("teste")
+
+  //   return response.end()
+
+// })
+
+// server.listen(8000)
+
+import { fastify } from 'fastify';
+// import { DatabaseMemory } from './database-memory.js';
+import { DatabasePostgres } from './database-postgres.js';
+const server = fastify()
+const database = new DatabasePostgres()
+
+server.post('/videos', async (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  await database.create({
+    title,
+    description,
+    duration,
+  })
+  console.log(database.list())
+  return reply.status(201).send()
+})
+
+server.get('/videos', async () => {
+  const videos = await database.list()
+  return videos
+})
+
+//route param (:id)
+server.put('/videos/:id', async (request, reply) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+
+  await database.update(videoId, {
+    title,
+    description,
+    duration,
+  })
+
+  return reply.status(204).send
+})
+
+server.delete('/videos/:id', async (request, reply) => {
+  const videoId = request.params.id;
+  await database.delete(videoId)
+
+  return reply.status(204).send
+})
+
+server.listen({
+  port: process.env.port ?? 8000
+})
